da22f097b33c01ee370d29876db3f27f06be15eb
João Vitor Da Silva De Oliveira <112949132+joaoOliveira09@users.noreply.github.com>
2023-12-13 09:46:04 -0300

    Estrutura basica do projeto

---
+++ ParkHub/src/main/java/br/ufsm/csi/ParkHub/ParkHubApplication.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.ParkHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkHubApplication.class, args);
+	}
+
+}
---
+++ ParkHub/src/test/java/br/ufsm/csi/ParkHub/ParkHubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufsm.csi.ParkHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
